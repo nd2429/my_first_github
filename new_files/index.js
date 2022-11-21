426b9791f247d365d5a5acf97e7a8ffd035ffb05
Nd2429 <nataliedom1987@gmail.com>
2022-11-21 16:07:09 -0500

    my first commit

---
+++ new_files/index.js
@@ -0,0 +1,7 @@
+const dogList = ['shitzhu', 'yorkie', 'pitbull', 'greyhound'];
+
+const catList = ['siamese', 'tabby', 'black', 'bengal']
+
+const myAnimalsList = dogList.concat(catList);
+
+console.log(myAnimalsList)
